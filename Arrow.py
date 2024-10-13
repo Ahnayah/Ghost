9a646197bc7a7483757abe218854ea80924b1526
Ahnayah <124970174+Ahnayah@users.noreply.github.com>
2024-10-13 05:08:30 -0500

    Add files via upload

--- Arrow.py
+++ Arrow.py
@@ -1,10 +1,10 @@
 import pygame
 
 class Arrow(pygame.sprite.Sprite):
-    def __init__(self, pos, speed = -8,screen_height = 400):
+    def __init__(self, pos, speed=-8, screen_height=400):
         super().__init__()
         self.image = pygame.image.load("assets/arrow.png").convert_alpha()
-        self.rect = self.image.get_rect(center = pos)
+        self.rect = self.image.get_rect(center=pos)
         self.speed = speed
         self.height_y_constraint = screen_height
     
--- Player.py
+++ Player.py
@@ -5,12 +5,12 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.image = pygame.image.load("assets/mainc.png").convert_alpha()
-        self.rect = self.image.get_rect(midbottom = pos)
+        self.rect = self.image.get_rect(midbottom=pos)
         self.speed = 5
         self.ready = True
         self.shoot_time = 0
         self.shoot_cooldown = 600
-        self.arrows  = pygame.sprite.Group()
+        self.arrows = pygame.sprite.Group()
         
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -27,17 +27,28 @@ class Player(pygame.sprite.Sprite):
             self.shoot_arrow()
             self.ready = False
             self.shoot_time = pygame.time.get_ticks()
+        
+        # Constrain player within window sides
+        screen_rect = pygame.display.get_surface().get_rect()
+        if self.rect.left < screen_rect.left:
+            self.rect.left = screen_rect.left
+        if self.rect.right > screen_rect.right:
+            self.rect.right = screen_rect.right
+        if self.rect.top < screen_rect.top:
+            self.rect.top = screen_rect.top
+        if self.rect.bottom > screen_rect.bottom:
+            self.rect.bottom = screen_rect.bottom
+    
     def recharge(self):
         if not self.ready:
             current_time = pygame.time.get_ticks()
             if current_time - self.shoot_time >= self.shoot_cooldown:
                 self.ready = True
-                
-        self.rect.clamp_ip(pygame.display.get_surface().get_rect())
+    
     def shoot_arrow(self):
         self.arrows.add(Arrow(self.rect.center, -8, self.rect.bottom)) 
     
-    def update(self):
+    def update(self, arrows):        
         self.get_input()
         self.recharge()
         self.arrows.update()
